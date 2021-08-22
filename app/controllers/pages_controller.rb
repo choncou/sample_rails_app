4c0caf648c3a2c18e51438b3304bb141b85e4267
Unathi Chonco <choncou@gmail.com>
2021-08-22 18:23:58 +0200

    Add random error on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
     CreateRandomPostsJob.perform_later
+
+    # Randomly force an error by creating an empty Post
+    if SecureRandom.rand * 100 > 85
+      Post.create!
+    end
   end
 end
