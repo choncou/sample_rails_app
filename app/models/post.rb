e1a131a9aeb5cd2c440a384961ee27d2a147d062
Unathi Chonco <choncou@gmail.com>
2021-09-21 20:27:35 +0200

    Sleep less during querying

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
 
   def self.where_title_starts_with(letter)
     Analytics.track_post_title_search(letter.downcase)
-    select('*, pg_sleep(1)').where("title ILIKE :letter", letter: "#{letter.downcase}%")
+    select('*, pg_sleep(0.1)').where("title ILIKE :letter", letter: "#{letter.downcase}%")
   end
 end
