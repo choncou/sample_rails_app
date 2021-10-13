184508d3d72040fe9f7f65a6b6fa9c917b9efcb6
Unathi Chonco <choncou@gmail.com>
2021-10-13 20:13:24 +0200

    Fix analytics service typo

--- app/services/analytics.rb
+++ app/services/analytics.rb
@@ -1,5 +1,5 @@
 class Analytics
-  def self.track_post_title_search(letter, , sleep = sleep(1))
+  def self.track_post_title_search(letter, sleep = sleep(1))
     # Some heavy processing
     sleep 1
   end
